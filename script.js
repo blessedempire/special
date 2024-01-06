113c1f995f7412709b16e2ff4105926216393bbb
Emediong Peter <brightpeter47@gmail.com>
2024-01-06 08:34:26 +0100

    Added

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log(planetName); // Earth
 
 let ourSolarSystem = "Milky Way";
 console.log(ourSolarSystem); // Milky Way
+
+let n = 123;
+console.log(n)
+
+const x = 1234567890123456789012345678901234567890n;
+console.log(x)
+
+let isGreater = 1 > 2
+alert(isGreater)
